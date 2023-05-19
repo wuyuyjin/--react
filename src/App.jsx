9fb6678f05818a3613257e214c9cbb37db56e6a5
wuyujin <3075385315@qq.com>
2023-05-19 14:51:57 +0800

    聊天室

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {IconArrowLeft} from '@tabler/icons-react';
+import {IconArrowAutofitContent} from '@tabler/icons-react';
+import {Box} from "@chakra-ui/react";
+
+const App = () => {
+    return (
+        <Box>
+            <IconArrowLeft/>
+            <IconArrowAutofitContent/>
+        </Box>
+    );
+};
+
+export default App;
